86713880a5a36b87f768c7ade303fbba7c2bc338
Valentyn N Sichkar <34546286+sichkar-valentyn@users.noreply.github.com>
2018-01-15 15:38:01 +0300

    Uploading file
    Adding file into the repository

---
+++ sketch_oct04a/sketch_oct04a.ino
@@ -0,0 +1,25 @@
+int analogPin = 1;
+float R1 = 0;
+
+void setup()
+{
+  pinMode(7, OUTPUT);
+  pinMode(8, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  R1 = analogRead(analogPin);
+
+  Serial.println(R1);
+
+  if(R1 > 984) digitalWrite(7, HIGH);
+  else digitalWrite(7, LOW);
+
+  if(R1 < 985) digitalWrite(8, HIGH);
+  else digitalWrite(8, LOW);
+
+  delay(1000);
+}
+
